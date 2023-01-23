c148a84ca7ea4f8a4d46e35ddf1e39a1238fe2c7
Przemysław Soliński <solinskip107@gmail.com>
2023-01-23 21:52:37 +0100

    display list when added an item, border, counter

--- to do list with ChatGPT/main.js
+++ to do list with ChatGPT/main.js
@@ -1,28 +1,42 @@
+let counter = 1;
 function addItem() {
   // Get the value of the input field
-  const item = document.getElementById('item').value;
+  const item = document.getElementById("item").value;
 
   // Add the item to the list
-  const list = document.getElementById('todo-list');
-  const newItem = document.createElement('li');
-  // const ul = document.querySelector('.ul');
-  newItem.innerHTML = item;
+  const list = document.getElementById("todo-list");
+  const newItem = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.className = "checkbox";
+  newItem.appendChild(checkbox);
+  newItem.innerHTML += item;
   list.appendChild(newItem);
+
+  // const ul = document.querySelector('.ul');
+  newItem.innerHTML = counter + ". " + item;
+  newItem.classList.add("list-item-border");
+  list.appendChild(newItem);
+  counter++;
+
   // ul.classList.add('active');
+  checkbox.addEventListener("click", function () {
+    newItem.classList.toggle("done");
+  });
 
   // Clear the input field
-  document.getElementById('item').value = '';
+  document.getElementById("item").value = "";
 }
 
 function addItemOnEnter(event) {
   // Check if the Enter key was pressed
-  if (event.key === 'Enter') {
+  if (event.key === "Enter") {
     addItem();
   }
 }
 
 function clearList() {
   // Remove all of the items from the list
-  var list = document.getElementById('todo-list');
-  list.innerHTML = '';
+  var list = document.getElementById("todo-list");
+  list.innerHTML = "";
 }
